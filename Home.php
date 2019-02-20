bf708647edf5fd840c2a0082e775b52d341a48db
themilanguy1 <milan.gupta@ymail.com>
2019-02-20 12:03:59 +0100

    2nd coommit

--- Home.php
+++ Home.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="Main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-xl-12 auto-center">
                 <h3>hallo</h3>
+                <a href="Login.php" class="btn btn-primary">Log Hier In, Sukkeltje ;)</a>
             </div>
             <div class="col-md-12">
                 <br>
@@ -21,20 +22,22 @@
                 <?php
                 require_once('Database.php');
                 foreach (Database::FetchProducts() as $row) {
-                    echo "<div class='col-md-4'>";
-                    echo "<div class='productdoos'>";
-                    echo "product: ".$row['naam_product'];
-                    echo "<br />";
-                    echo "prijs: ".$row['prijs'];
-                    $afbeelding = $row['afbeelding'];
-
-                    echo "<img src='$afbeelding'/>";
-                    echo "</div>";
-                    echo "</div>";
-                }
 
                 ?>
+            <div class='col-md-4'>
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top align-self-center" src=" <?php echo $row['product_afbeelding'] ?> " style="width:150px;height:150px;">
+                    <div class="card-body">
+                        <h5 class="card-title"> <?php echo $row['product_naam']?> </h5>
+                        <p class="card-text"> <?php echo $row['product_prijs']?> </p>
+                        <p class="card-text"> <?php echo $row['product_prijs']?> </p>
+                        <a href="#" class="btn btn-primary">Go Fuck Yourself, Thank You.</a>
+                    </div>
+                </div>
             </div>
+                <?php
+                }
+                ?>
         </div>
     </div
 </body>
