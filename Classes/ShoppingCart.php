422ea7e3ae547b89344087bc683869f7b4cffe87
themilanguy1 <milan.gupta@ymail.com>
2019-03-10 18:37:42 +0100

    made a table for shopping cart but adding still doesnt work

--- Classes/ShoppingCart.php
+++ Classes/ShoppingCart.php
@@ -12,14 +12,24 @@ class ShoppingCart
     public static function DisplayInventory()
     {
         if (isset($_SESSION['shopping_cart_inventory']) && (!empty($_SESSION['shopping_cart_inventory']))) {
+            echo "<table class='table'>";
+            echo "<thead class='thead-dark'>";
+            echo "<tr>";
+            echo "<th scope='col'>Product_id</th>";
+            echo "<th scope='col'>Aantal</th>";
+            echo "</tr>";
+            echo "<tbody>";
             foreach ($_SESSION['shopping_cart_inventory'] as $item) {
+                echo "<tr>";
                 if (is_array($item) || is_object($item)) {
-                    foreach ($item as  $key => $value) {
-                        echo $key . " " . $value;
-                        echo "<br />";
+                    foreach ($item as $key => $value) {
+                        echo "<td>$value</td>";
                     }
                 }
+                echo "</tr>";
             }
+            echo "</tbody>";
+            echo "</table>";
             return true;
         } else {
             echo "Winkelmand leeg.";
@@ -37,32 +47,43 @@ class ShoppingCart
     {
         if (!empty($product_id && $product_quantity)) {
             if (isset($_SESSION['shopping_cart_inventory']) && (!empty($_SESSION['shopping_cart_inventory']))) {
-                // check if product being added to cart already exists in cart ->
-                if (array_search($product_id, array_column($_SESSION['shopping_cart_inventory'], 'product_id'))) {
-//                    $_SESSION['shopping_cart_inventory']['product_quantity'] = $product_quantity;
-                } else {
+                if (array_search($product_id, array_column($_SESSION['shopping_cart_inventory'], 'product_id')) === true) {
+                    echo "yeet";
+//                    foreach ($item as $key => $value) {
+//                        if ($key == $product_id) {
+//                            echo $key;
+//                            $value = 21;
+//                        }
+//                    }
+                }
+                else {
                     $_SESSION['shopping_cart_inventory'][] = array(
-                        "product_id" => $product_id, "product_quantity" => $product_quantity
+                        "product_id" => $product_id,
+                        "product_quantity" => $product_quantity
                     );
                 }
             } else {
                 $_SESSION['shopping_cart_inventory'] = array(
-                    "product_id" => $product_id, "product_quantity" => $product_quantity
+                    array(
+                        "product_id" => $product_id,
+                        "product_quantity" => $product_quantity
+                    )
                 );
             }
         }
-        header('Location: Home.php');
+//        header('Location: Home.php');
     }
 
 
     public static function Remove()
     {
+        //placeholder
 
     }
 
 
     public static function EditQuantity()
     {
-
+        //placeholder
     }
 }
